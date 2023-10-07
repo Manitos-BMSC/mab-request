033dca5793b0aa10c165a632e7072b4208568d93
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-07 17:22:55 -0400

    first commit

---
+++ src/main/java/bo/edu/ucb/mabrequest/MabRequestApplication.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.mabrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/edu/ucb/mabrequest/MabRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.mabrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
